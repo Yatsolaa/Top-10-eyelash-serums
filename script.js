7b435afccef9fdaf3f0ba56f25e700c9379b8f62
Yatsolaa <andry.yatsola@gmail.com>
2021-11-30 00:38:08 +0200

    corrected header, info-block, add fade out

--- script.js
+++ script.js
@@ -46,8 +46,32 @@ function scrollFunction() {
   }
 }
 
+//fade out on scroll
 
+var header = document.getElementById('hero-text');
 
+function fadeOutOnScroll(element) {
+  if (!element) {
+    return;
+  }
+  
+  var distanceToTop = window.pageYOffset + element.getBoundingClientRect().top;
+  var elementHeight = element.offsetHeight;
+  var scrollTop = document.documentElement.scrollTop;
+  
+  var opacity = 1;
+  
+  if (scrollTop > distanceToTop) {
+    opacity = 1 - (scrollTop - distanceToTop) / elementHeight;
+  }
+  
+  if (opacity >= 0) {
+    element.style.opacity = opacity;
+  }
+}
 
+function scrollHandler() {
+  fadeOutOnScroll(header);
+}
 
-
+window.addEventListener('scroll', scrollHandler);
